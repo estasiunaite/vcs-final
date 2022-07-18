c2920e1a728b334a140b339648a543c8c06f7382
estasiunaite <eglestasiunaite@gmail.com>
2022-07-18 16:26:08 +0300

    Uploaded elements to Rooms

--- src/Components/Rooms/Rooms.jsx
+++ src/Components/Rooms/Rooms.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import "./rooms.css";
 import PriceCard from "../../Shared/PriceCard/PriceCard";
+import ThemeDividerVertical from "../../Shared/ThemeDividerVertical/ThemeDividerVertical";
+import room1bw from "../../Assets/images/bw/room1bw.jpg";
+import room2bw from "../../Assets/images/bw/room2bw.jpg";
+import room3bw from "../../Assets/images/bw/room3bw.jpg";
+import room1 from "../../Assets/images/color/room1.jpg";
+import room2 from "../../Assets/images/color/room2.jpg";
+import room3 from "../../Assets/images/color/room3.jpg";
 
 const Rooms = () => {
   return (
-    <div className="rooms">
+    <div className="roomsSection">
       <div className="container container__rooms">
-        <div ClassName="sectionDivider">
-          <h1>rooms</h1>
-          <div class="line-2">
-            <hr></hr>
-          </div>
+        <div className="themeDividerVerticalRooms">
+          <ThemeDividerVertical />
         </div>
         <div className="roomPrices">
           <PriceCard
@@ -18,6 +22,20 @@ const Rooms = () => {
             roomType="FAMILY room"
             roomDescription="Family rooms can accommodate 4 guests. If you travel with children under 3 we offer a complimentary baby cot."
           />
+        </div>
+
+        <div roomTypes>
+          <h3>Double room</h3>
+          <h3>shared room</h3>
+        </div>
+
+        <div className="roomPictures">
+          <img className="room1bw" src={room1bw} alt="" />
+          <img className="room2bw" src={room2bw} alt="" />
+          <img className="room3bw" src={room3bw} alt="" />
+          <img className="room1" src={room1} alt="" />
+          <img className="room2" src={room2} alt="" />
+          <img className="room3" src={room3} alt="" />
         </div>
       </div>
     </div>
---
+++ src/Shared/ThemeDividerVertical/ThemeDividerVertical.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./ThemeDividerVertical.css";
+
+const ThemeDividerVertical = () => {
+  return (
+    <div ClassName="themeDividerVert">
+      <div className="themeName">
+        <h1>rooms</h1>
+      </div>
+      <div class="line-2">
+        <hr></hr>
+      </div>
+    </div>
+  );
+};
+
+export default ThemeDividerVertical;
