5bb05ab8f453015cda654744a712bbb06393b58b
estasiunaite <eglestasiunaite@gmail.com>
2022-07-21 18:07:12 +0300

    Updated Activities Section

--- src/Components/Activities/Activities.jsx
+++ src/Components/Activities/Activities.jsx
@@ -8,30 +8,29 @@ import lobby from "../../Assets/images/color/lobby.jpg";
 const Activities = () => {
   return (
     <div className="activitiesSection ">
-      {/* <div className="themeDividerVerticalActivities">
-        <ThemeDividerVertical themeName="Activities" />
-      </div> */}
       {/* <div className="container activities__container"> */}
-
       <div className="activitiesRec">
-        <div className="container activities__container">
-          <div className="aboutActivities">
-            <h2>gathering place</h2>
-            <p>
-              This is a space for relaxing in a hammock or on the sofa, plugging
-              into the internet with your personal laptop, sitting down to a
-              game and eating dinner to the sunset. Enjoy pleasant breezes and
-              incredible views of the lake, forest and evening sunsets.
-            </p>
-            <div className="learnMoreBtnActivities">
-              <LearnMoreBtn />
-            </div>
-          </div>
+        <div className="themeDividerVerticalActivities">
+          <ThemeDividerVertical themeName="Activities" />
         </div>
-      </div>
+      </div>{" "}
       <div className="lobbyPic">
         <img src={lobbybw} alt="" />
       </div>
+      {/* <div className="container activities__container"> */}
+      <div className="aboutActivities">
+        <h2>gathering place</h2>
+        <p>
+          This is a space for relaxing in a hammock or on the sofa, plugging
+          into the internet with your personal laptop, sitting down to a game
+          and eating dinner to the sunset. Enjoy pleasant breezes and incredible
+          views of the lake, forest and evening sunsets.
+        </p>
+        <div className="learnMoreBtnActivities">
+          <LearnMoreBtn />
+        </div>
+      </div>
+      {/* </div> */}
     </div>
     // </div>
   );
