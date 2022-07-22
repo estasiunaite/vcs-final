251b022dfddbd9b5526a1d6a3e35554555c0ffcb
estasiunaite <eglestasiunaite@gmail.com>
2022-07-22 14:37:24 +0300

    Updated Gallery Section

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,38 +1,28 @@
 import React from "react";
-
 import "./gallery.css";
-
-// import { FcNext } from "react-icons/fc";
-import Slide from "../../Shared/Slide/Slide";
 
 import ThemeDividerHorizontal from "../../Shared/ThemeDividerHorizontal/ThemeDividerHorizontal";
 import ImageCreekBw from "../../Assets/images/bw/creekbw.jpg";
 import ImageLakeBw from "../../Assets/images/bw/lakebw.jpg";
 import ImageForestBw from "../../Assets/images/bw/forestbw.jpg";
+import ImageSlider from "./ImageSlider";
 
 const Gallery = () => {
+  const slides = [
+    { url: ImageCreekBw },
+    { url: ImageLakeBw },
+    { url: ImageForestBw },
+  ];
   return (
     <div className="galleryContainer">
       <div className="themeDividerHorizontalgallery">
         <ThemeDividerHorizontal themeName="gallery" />
       </div>
-      <div className="sliderContainer"></div>
+      <div className="container slidesContainer">
+        <ImageSlider slides={slides} />
+      </div>
     </div>
   );
 };
 
 export default Gallery;
-
-{
-  /* <Swiper>
-          <SwiperSlide>
-            <Slide slideImgLink={ImageCreekBw} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Slide slideImgLink={ImageLakeBw} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Slide slideImgLink={ImageForestBw} />
-          </SwiperSlide>
-        </Swiper> */
-}
---
+++ src/Components/Gallery/ImageSlider.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useState } from "react";
+import "./ImageSlider.css";
+import { FcNext } from "react-icons/fc";
+
+const ImageSlider = ({ slides }) => {
+  const [currentIndex, setcurrentIndex] = useState(0);
+  const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setcurrentIndex(newIndex);
+  };
+  const goToNext = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setcurrentIndex(newIndex);
+  };
+
+  return (
+    <div className="imageSlider">
+      <div className="arrowLeft" onClick={goToPrevious}>
+        {"\u2770"}
+      </div>
+      <div className="arrowRight" onClick={goToNext}>
+        {"\u2771"}
+      </div>
+      <div
+        className="imageSlides"
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+      ></div>
+    </div>
+  );
+};
+
+export default ImageSlider;
