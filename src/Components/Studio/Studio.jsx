2bd7fc252503f128fbe9f83584eb7a8020982c9e
estasiunaite <eglestasiunaite@gmail.com>
2022-07-27 15:36:52 +0300

    Updated Studio section

--- src/Components/Studio/Studio.jsx
+++ src/Components/Studio/Studio.jsx
@@ -2,39 +2,32 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import "./Studio.css";
-import ThemeDividerVerticalLeft from "../../Shared/ThemeDividerVerticalLeft/ThemeDividerVerticalLeft";
-import ThemeDividerHorizontal from "../../Shared/ThemeDividerHorizontal/ThemeDividerHorizontal";
 import LearnMoreBtn from "../../Shared/LearnMoreBtn";
 import sutdioPicbw from "../../Assets/images/bw/sutdioPicbw.jpg";
 // import lobby from "../../Assets/images/color/lobby.jpg";
 
 const Studio = () => {
   return (
-    <div id="toStudio">
-      <div className="themeDividerHorizontalStudio">
-        <ThemeDividerHorizontal themeName="Studio" />
-      </div>
-      {/* <div className="container activities__container"> */}
-      <div className="environmentRec">
-        <div className="themeDividerVerticalStudio">
-          <ThemeDividerVerticalLeft themeName="Studio" />
-        </div>
-      </div>
-      <div className="lobbyPic">
+    <div id="toStudio" className="studioContainer">
+      <h1 className="themeDividerVerticalStudio">Studio</h1>
+      <div className="environmentRec"></div>
+      <div className="studioPic">
         <img src={sutdioPicbw} alt="" />
       </div>
-      <div className="aboutEnvironment ">
-        <h2> Cosy Environment</h2>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam
-          possimus quaerat omnis, optio quo repellat id maxime ipsa consectetur.
-          Adipisci provident animi quas consectetur facere doloremque a vero,
-          dignissimos soluta voluptatem.
-        </p>
-        <div className="learnMoreBtnStudio">
-          <Link to="/StudioPage">
-            <LearnMoreBtn />
-          </Link>
+      <div className="aboutEnvironment1">
+        <div className="aboutEnvironment">
+          <h2> Cosy Environment</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam
+            possimus quaerat omnis, optio quo repellat id maxime ipsa
+            consectetur. Adipisci provident animi quas consectetur facere
+            doloremque a vero, dignissimos soluta voluptatem.
+          </p>
+          <div className="learnMoreBtnStudio">
+            <Link to="/StudioPage">
+              <LearnMoreBtn />
+            </Link>
+          </div>
         </div>
       </div>
     </div>
